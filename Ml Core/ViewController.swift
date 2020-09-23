18d86f6b948fc2306de8711a43c112af9afdbc22
ahmednasr8898 <ans96283@gmail.com>
2020-09-23 15:49:45 -0700

    Initial Commit

---
+++ Ml Core/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ml Core
+//
+//  Created by Ahmed Nasr on 9/23/20.
+//  Copyright © 2020 Ahmed Nasr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
