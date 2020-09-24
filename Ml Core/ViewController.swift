72c42c88acdb87cd9a350fb47b85c06454d25f1f
ahmednasr8898 <ans96283@gmail.com>
2020-09-23 18:11:26 -0700

    Add_MLCore_Project

--- Ml Core/ViewController.swift
+++ Ml Core/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var ShowLabel: UILabel!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let imagePickerView = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+
+            imageView.image = imagePickerView
+        
+        
+        guard let ciImage = CIImage(image: imagePickerView) else {
+            
+            fatalError("ERROR")
+        
+            }
+            
+            detect(image: ciImage)
+        }
     }
 
-
+    @IBAction func openGallaryOnClick(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker , animated:  true ,completion: nil)
+    }
+    
+    
+    func detect(image: CIImage){
+      
+        guard  let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+                fatalError("Core ML loading failed")
+        }
+         
+        let request = VNCoreMLRequest(model: model ) {   (request, error ) in
+       guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Core ML process failed")
+             }
+          //  print(results)
+            if let fisrtresult = results.first {
+                if fisrtresult.identifier.contains("desktop computer"){
+                    self.ShowLabel.text = "desktop computer"
+                }else{
+                    self.ShowLabel.text = "Not computer"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+           do {
+               try handler.perform([request])
+           } catch {
+               print("Failed to perform classification.\n\(error.localizedDescription)")
+           }
+    }
 }
-
